ba4403d9dd22a77d1e282c546ca0d7a28102712f
jackzhang <48399041+chasteAugus@users.noreply.github.com>
2021-01-08 23:39:43 +0800

    Update Hello-World in C.c

--- C/Hello-World in C.c
+++ C/Hello-World in C.c
@@ -1,13 +1,10 @@
 #include<stdlib.h>
 #include<stdio.h>
-
 int main(){
-
-int i;
-printf("Hello World");
-
-for(i=0;i<3;i++){
-printf("Nice to meet you");
+  int i;
+  printf("Hello World");
+  for(i=0;i<3;i++){
+    printf("Nice to meet you");
+  }
+  return 0;
 }
-
-return 0;}
